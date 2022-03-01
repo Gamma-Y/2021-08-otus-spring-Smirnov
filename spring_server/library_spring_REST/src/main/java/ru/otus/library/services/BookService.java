80eb6abdfa311e9bebbd83b0bbce2ff7a51de3ad
Gamma-Y <160397jo@mail.ru>
2022-03-01 22:26:52 +0500

    add hystrix

--- spring_server/library_spring_REST/src/main/java/ru/otus/library/services/BookService.java
+++ spring_server/library_spring_REST/src/main/java/ru/otus/library/services/BookService.java
@@ -1,10 +1,13 @@
 package ru.otus.library.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.library.controllers.dto.BookDTO;
 import ru.otus.library.database.entities.Author;
 import ru.otus.library.database.entities.Book;
+import ru.otus.library.database.entities.Comment;
 import ru.otus.library.database.entities.Genre;
 import ru.otus.library.database.repositories.AuthorRepository;
 import ru.otus.library.database.repositories.BookRepository;
@@ -27,6 +30,12 @@ public class BookService {
         return books;
     }
 
+    @HystrixCommand(fallbackMethod = "buildFallbackBooksDTO", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public List<BookDTO> getAllHowDTO() {
         List<Book> books = this.getAll();
         List<BookDTO> booksDTO = new ArrayList<>();
@@ -36,11 +45,23 @@ public class BookService {
         return booksDTO;
     }
 
+    @HystrixCommand(fallbackMethod = "buildFallbackBook", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public Book getById(long id) {
         Book book = repository.findById(id).get();
         return book;
     }
 
+    @HystrixCommand(fallbackMethod = "returnStatus", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public boolean deleteById(long id) {
         Book book = repository.findById(id).get();
         repository.delete(book);
@@ -61,8 +82,14 @@ public class BookService {
         repository.save(book);
     }
 
+    @HystrixCommand(fallbackMethod = "returnStatus", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public boolean deleteGenreFromBook(long bookId, long genreId) {
-        Book book = repository.getById(bookId);
+        Book book = repository.getOne(bookId);
         List<Genre> genres = book.getGeneris();
         for (Genre genre : genres) {
             if (genre.getId() == genreId) {
@@ -74,8 +101,14 @@ public class BookService {
         return true;
     }
 
+    @HystrixCommand(fallbackMethod = "returnStatus", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public boolean deleteAuthorFromBook(long bookId, long authorId) {
-        Book book = repository.getById(bookId);
+        Book book = repository.getOne(bookId);
         List<Author> authors = book.getAuthors();
         for (Author author : authors) {
             if (author.getId() == authorId) {
@@ -87,8 +120,14 @@ public class BookService {
         return true;
     }
 
+    @HystrixCommand(fallbackMethod = "buildFallbackBookDTO", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public BookDTO updateBook(BookDTO bookDTO) {
-        Book book = repository.getById(bookDTO.getId());
+        Book book = repository.getOne(bookDTO.getId());
         book.setName(bookDTO.getName());
         return convertBookToDTO(repository.save(book));
     }
@@ -109,5 +148,39 @@ public class BookService {
         }
         bookDTO.setGenres(genres);
         return bookDTO;
+    }
+
+    public List<BookDTO> buildFallbackBooksDTO() {
+        System.out.println("test");
+        return List.of(buildFallbackBookDTO());
+    }
+
+    public BookDTO buildFallbackBookDTO() {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setName("N/A");
+        bookDTO.setId(-1);
+        bookDTO.setAuthors(List.of("N/A"));
+        bookDTO.setGenres(List.of("N/A"));
+
+        return bookDTO;
+    }
+
+    public BookDTO buildFallbackBookDTO(BookDTO oldBookDTO) {
+        BookDTO bookDTO = buildFallbackBookDTO();
+        return bookDTO;
+    }
+
+    public Book buildFallbackBook(BookDTO oldBookDTO) {
+        Book book = new Book();
+        book.setName("N/A");
+        book.setId(-1);
+        book.setAuthors(List.of(new Author("N/A")));
+        book.setComments(List.of(new Comment("N/A", -1)));
+        book.setGeneris(List.of(new Genre("N/A")));
+        return book;
+    }
+
+    public boolean returnStatus(long... id) {
+        return false;
     }
 }
